f2db143d9147d6fc72279bce498d0db817bb7432
uaimio <mario.acocella@outlook.com>
2024-09-21 22:52:48 +0200

    aggiunti findAll e upload su unsecure controller resourceserver

--- resourceserver/src/main/java/com/oauth2/resourceserver/controller/UnsecureController.java
+++ resourceserver/src/main/java/com/oauth2/resourceserver/controller/UnsecureController.java
@@ -1,6 +1,7 @@
 package com.oauth2.resourceserver.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.oauth2.resourceserver.model.Element;
+import com.oauth2.resourceserver.model.dto.ElementDTO;
 import com.oauth2.resourceserver.service.PdfService;
 
 @RestController
@@ -25,13 +28,12 @@ public class UnsecureController {
     private PdfService pdfService;
 
     @PostMapping("/upload")
-    public ResponseEntity<String> uploadPdf(@RequestParam("file") MultipartFile file) {
-        try {
-            ObjectId id = pdfService.storePdf(file);
-            return new ResponseEntity<>(id.toHexString(), HttpStatus.OK);
-        } catch (IOException e) {
-            return new ResponseEntity<>("Failed to upload PDF", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<ElementDTO> insertDocument(@RequestPart MultipartFile file) throws IOException {
+        Element element = pdfService.save(file);
+        if (element != null)
+            return new ResponseEntity<>(new ElementDTO(element), HttpStatus.ACCEPTED);
+        else
+            return new ResponseEntity<>((ElementDTO) null, HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping("/download/{id}")
@@ -50,26 +52,8 @@ public class UnsecureController {
         }
     }
 
-    // @GetMapping("/download")
-    // public ResponseEntity<byte[]> downloadPdf() {
-    //     // try {
-    //         // ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-    //         // Document document = new Document();
-    //         // PdfWriter writer = PdfWriter.getInstance(document, byteArrayOutputStream);
-    //         // // PdfDocument pdfDoc = new PdfDocument(writer);
-            
-    //         // document.add(new Paragraph("Hello, this is a PDF document created by iText."));
-    //         // document.close();
-
-    //         // byte[] pdfBytes = byteArrayOutputStream.toByteArray();
-    //         byte[] pdfBytes = {};
-    //         HttpHeaders headers = new HttpHeaders();
-    //         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=document.pdf");
-    //         headers.add(HttpHeaders.CONTENT_TYPE, "application/pdf");
-
-    //         return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
-    //     // } catch (DocumentException e) {
-    //     //     return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-    //     // }
-    // }
+    @GetMapping("/findAll")
+    public List<ElementDTO> getAllDocuments() {
+        return pdfService.getMetadataAllDocuments().stream().map(ElementDTO::new).toList();
+    }
 }
